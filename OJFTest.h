441880e6b29277197babbe26a988da21c69facfb
Oliver Feldman <oliverfeldman@Olivers-MacBook-Air.local>
2014-08-24 22:02:39 +0200

    added a test class

---
+++ OJFTest.h
@@ -0,0 +1,13 @@
+//
+//  OJFTest.h
+//  Git Test
+//
+//  Created by Oliver Feldman on 24/08/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OJFTest : NSObject
+
+@end
---
+++ OJFTest.m
@@ -0,0 +1,13 @@
+//
+//  OJFTest.m
+//  Git Test
+//
+//  Created by Oliver Feldman on 24/08/14.
+//
+//
+
+#import "OJFTest.h"
+
+@implementation OJFTest
+
+@end
